393c3d79de74e7db6e39a6fcc9ffc6b4118d9b6b
Padmavathy G <padmasri1502@gmail.com>
2023-12-26 16:00:20 +0530

    commit message

---
+++ Fontend/src/app/model/user.ts
@@ -0,0 +1,34 @@
+export class User {
+  userid: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+  profileImage:string;
+  address:string;
+  district:string;
+  state:string;
+  userRole!: string;
+
+  constructor( userid: number,first_name: string,last_name: string,username: string,
+      password: string,email: string,phoneNumber: string,
+      profileImage:string,
+      address:string,
+      district:string,
+      state:string) {
+          this.userid = userid;
+          this.first_name = first_name;
+          this.last_name = last_name;
+          this.username = username;
+          this.password = password;
+          this.email = email;
+          this.phoneNumber= phoneNumber;
+          this.profileImage=profileImage;
+          this.address=address;
+          this.district=district;
+          this.state=state;
+
+  }
+}
